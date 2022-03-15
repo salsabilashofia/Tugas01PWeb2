27c63965afc67e195360eb3f2da3e1420334e500
salsabilashofia <101611648+salsabilashofia@users.noreply.github.com>
2022-03-15 10:19:04 +0800

    Add files via upload

---
+++ tugas01.php
@@ -0,0 +1,27 @@
+<form action="" method="POST">
+      Masukkan Nilai : <input type="text" name="nilai">
+      <input type="submit" value="Hasil">
+    </form>
+    <?php
+      if ($_POST)
+      {
+        $nilai = $_POST['nilai'];
+        if ($nilai >= 80)
+        {
+          $grade = "A";
+        }
+        elseif ($nilai >= 70)
+        {
+          $grade = "B";
+        }
+        elseif ($nilai >= 60)
+        {
+          $grade = "C";
+        }
+        else
+        {
+          $grade = "D";
+        }
+        echo "Huruf : " . $grade;
+      }
+    ?>

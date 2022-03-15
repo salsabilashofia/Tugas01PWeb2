f1beaea89a103f03fd6f87bc25a7ef11cd478d0b
salsabilashofia <101611648+salsabilashofia@users.noreply.github.com>
2022-03-15 10:32:25 +0800

    Update tugas01.php

--- tugas01.php
+++ tugas01.php
@@ -1,7 +1,5 @@
 <form action="" method="POST">
-      Masukkan Nilai : <input type="text" name="nilai">
-      <input type="submit" value="Hasil">
-    </form>
+   Masukkan Nilai : <input type="text" name="nilai">
     <?php
       if ($_POST)
       {
